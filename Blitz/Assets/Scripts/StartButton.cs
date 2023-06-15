a80f8b738b7d84917f24e525366f0f388fdbb5d2
ShortS92 <samantharshort92@yahoo.com>
2023-06-14 20:15:03 -0400

    Creation of scenes
    and stuff

--- Blitz/Assets/Scripts/StartButton.cs
+++ Blitz/Assets/Scripts/StartButton.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class StartButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
