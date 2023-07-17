576991fd1c52da7592b12ca1dde3d3040856d700
YuWei Chang <113270725+ywchang29@users.noreply.github.com>
2023-07-17 19:57:13 -0400

    Add files via upload

---
+++ Blitz/Assets/Scripts/ObstaclesController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesController : MonoBehaviour
+{
+    public float speed = 500f;
+
+    void Update()
+    {
+
+        Vector3 temp = transform.position;
+        temp.x -= speed * Time.deltaTime;
+        transform.position = temp;
+
+        if (transform.position.x < -1025)
+            gameObject.SetActive(false);
+
+    }
+
+
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
